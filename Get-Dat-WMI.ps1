91d9bf606f0796c178d42b7b653dc49581e0e5b5
The-Honey-Badger <30511149+The-Honey-Badger@users.noreply.github.com>
2018-05-26 15:02:09 -0400

    And so it begins.
    First rev. Needs error checking.

---
+++ Get-Dat-WMI.ps1
@@ -0,0 +1,12 @@
+﻿$OutFile = "C:\temp\ALL-DA-WMI-"+(get-date -Format MMddyyyy:HHmm)+".txt"
+
+Write-Host Saving to -> $OutFile
+
+$Data = ForEach ($i in (Get-WmiObject -List | Select-Object Name)) {
+
+    echo "Current Class-> " $i.Name | Out-File $OutFile -Append -NoClobber
+    Get-WmiObject $i.Name | Out-File $OutFile -Append -NoClobber
+      
+}
+
+$Data
