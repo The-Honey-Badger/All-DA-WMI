0926b67d9eb0c3528099c4bc2413bfb6ed9cee0d
KSI Syn <celticruins@gmail.com>
2018-05-29 10:33:03 -0400

    Update Get-Dat-WMI.ps1
    Add some comments and update format for writing start of each WMI object to file. 
    
    Update not yet tested!

--- Get-Dat-WMI.ps1
+++ Get-Dat-WMI.ps1
@@ -1,12 +1,19 @@
-﻿$OutFile = "C:\temp\ALL-DA-WMI-"+(get-date -Format MMddyyyy:HHmm)+".txt"
+# Set New Line format to support MS Notepad
+$NLCR = "`r`n"
 
+# Set Output path and Filename
+$OutFile = "C:\temp\ALL-DA-WMI-"+(get-date -Format MMddyyyy:HHmm)+".txt"
+
+# Write Filename and Path to screen output 
 Write-Host Saving to -> $OutFile
 
-$Data = ForEach ($i in (Get-WmiObject -List | Select-Object Name)) {
+# Get Current list of all WMI objects and itterate over name 
+ForEach ($i in (Get-WmiObject -List | Select-Object Name)) {
 
-    echo "Current Class-> " $i.Name | Out-File $OutFile -Append -NoClobber
+    # Write current class and time to output file
+    echo "Current Class-> " $i.Name + $NLCR + "Current Time: " + (get-date -Format MMddyyyy:HHmm) + $NLCR | Out-File $OutFile -Append -NoClobber
+    
+    # Get WMI Object Properties and output to file
     Get-WmiObject $i.Name | Out-File $OutFile -Append -NoClobber
       
 }
-
-$Data
