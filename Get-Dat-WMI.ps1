8643a3da657b2369ad8cfb86f30cb7e31deb0a76
The-Honey-Badger <30511149+The-Honey-Badger@users.noreply.github.com>
2018-05-30 22:56:11 -0400

    Revised Error Log Output Format

--- Get-Dat-WMI.ps1
+++ Get-Dat-WMI.ps1
@@ -42,19 +42,19 @@ Write-Host Let us begin...
 ForEach ($i in (Get-WmiObject -List | Select-Object Name)) {
 
     # Write current class and time to output file
-    $txt= "Current Class-> "+$i.Name+$NLCR+"Current Time: "+(get-date -Format MMddyyyy_HHmm)+$NLCR |
+    $txt= "Current Class-> "+$i.Name+$NLCR+"Current Time: "+(get-date -Format "MM/dd/yyyy HH:mm:ss")+$NLCR |
         Out-File $OutFile -Append -NoClobber -encoding ASCII
     
 	try {
 		
 		# Get WMI Object Properties and output to file set EA action to stop to force terminating error on exception
 		Get-WmiObject -class $i.Name -EnableAllPrivileges -ErrorAction 'Stop'| Out-File $OutFile -Append -NoClobber -encoding ASCII
-	}
+    }
 
 	# Catch exceptions thrown by get wmi command
 	catch {
-		$txt = "ERROR : " + (get-date -Format MMddyyyy_HH:mm:ss) + " Exception caught for " + $i.Name + $NLCR + " Exception Detail : " + $Error[0].Exception
-		write-hoste $txt
+		$txt = (get-date -Format "MM/dd/yyyy HH:mm:ss") + " -- Exception caught for " + $i.Name + $NLCR + " Exception Detail : " + $Error[0].Exception + $NLCR
+		#write-host $txt
 		log -filename $ErrFile -text $txt
 	}  
 }
